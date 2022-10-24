769258f0d486c77351ed1c9283eb2cf8e3321be3
vagrant <vagrant@vagrant.vm>
2022-10-24 19:29:06 +0900

    somecommit

--- app/controllers/publics/bottles_controller.rb
+++ app/controllers/publics/bottles_controller.rb
@@ -2,5 +2,6 @@ class Publics::BottlesController < ApplicationController
 	def show
 		@bottle=Bottle.find(params[:id])
         @recommending_bottles=@bottle.followings
+        @tags=@bottle.tags
 	end
 end
